da14f8b9e5a137fa876de01a322e2383c4c44440
SayatSaguin <aeronsayat24@gmail.com>
2021-09-28 04:15:57 +0000

    2nd part/Complete Code

--- Laboratory3.py
+++ Laboratory3.py
@@ -19,3 +19,46 @@ t4=int(input("Enter Item Price 4: "))
 t5=int(input("Enter Item Price 5: "))
 
 total= t1 + t2 + t3 + t4 + t5
+
+if total>=5000 and total<8000:
+    dis = total * 0.05
+    com = total - dis
+    print("========================================================")  
+    print("5% discount applied")
+    print("Total Price :", com)
+    print("========================================================")   
+elif total>=8000 and total<10999:
+    dis = total * 0.1
+    com = total - dis
+    print("========================================================")  
+    print("10% discount applied")
+    print("Total Price :", com)
+    print("========================================================")   
+elif total>=11000 and total<13999:
+    dis = total * 0.2
+    com = total - dis
+    print("========================================================")  
+    print("20% discount applied")
+    print("Total Price :", com)
+    print("========================================================")   
+elif total>=14000 and total<16999:
+    dis = total * 0.25
+    com = total - dis
+    print("========================================================")  
+    print("25% discount applied")
+    print("Total Price :", com)
+    print("========================================================")   
+elif total>=17000:
+    dis = total * 0.3
+    com = total - dis
+    print("========================================================")  
+    print("30% discount applied")
+    print("Total Price :", com)
+    print("========================================================")   
+else:
+    print("========================================================")  
+    print("No Discount")
+    print("Total Price :", total)
+    print("========================================================")   
+    
+
